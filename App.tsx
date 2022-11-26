09711dadc0dd65017a83732f3c6db77acbbc2cec
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-11-26 09:54:51 +0100

    Feat: Base project

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+
+import {Header} from 'react-native/Libraries/NewAppScreen';
+import {Button} from 'rn-module';
+import {IUser, TCheck} from 'rn-module/src/typings';
+
+const App = () => {
+  const user: IUser = {firstName: 'stes', id: 'asd', lastName: 'tes'};
+  const check: TCheck = 123;
+  console.log('🚀 --- App --- user', user, check);
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <Header />
+        <View>
+          <Button />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ Readme.md
@@ -0,0 +1,1 @@
+// "rn-module": "/Users/santhosh-umapathi/Desktop/Playground/RN-Module"
