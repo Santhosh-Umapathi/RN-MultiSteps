8834ec6e045a927547503d93053d782853bb7c62
Santhosh Umapathi <Santhosh.Umapathi@AppyMango.com>
2022-11-28 06:55:58 +0100

    FEAT: Local module added

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {SafeAreaView, ScrollView, View} from 'react-native';
 
 import {Header} from 'react-native/Libraries/NewAppScreen';
 import {Button} from 'rn-module';
-import {IUser, TCheck} from 'rn-module/src/typings';
+import {IUser, TCheck} from 'rn-module/dist/typings';
 
 const App = () => {
   const user: IUser = {firstName: 'stes', id: 'asd', lastName: 'tes'};
